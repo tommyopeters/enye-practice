f984c70f76e63672edb4139163ea04c66b9d53ca
Tommy O' Peters <tommyopeters>
2021-02-06 09:06:56 +0100

    Design implementation

--- js/vue.js
+++ js/vue.js
@@ -5,6 +5,42 @@ let App = new Vue({
     profileGroups: [],
     page: 0,
   },
+  computed: {
+    firstPage: function () {
+      if (this.page == 0) {
+        return true
+      }
+      return false
+    },
+    lastPage: function () {
+      if (this.page + 1 == this.profileGroups.length) {
+        return true
+      }
+      return false
+    },
+    onlyPage: function () {
+      if (!this.profileGroups.length > 1) {
+        return true
+      }
+      return false
+    },
+  },
+  methods: {
+    prevPage: function () {
+      this.page--
+    },
+    nextPage: function () {
+      this.page++
+    },
+    genderFormat: function (gender) {
+      if (gender.toLowerCase() == "male") {
+        return "M"
+      } else if (gender.toLowerCase() == "female") {
+        return "F"
+      }
+      return "-"
+    },
+  },
   created() {
     axios.get("https://api.enye.tech/v1/challenge/records").then((res) => {
       console.log(res.data.records.profiles[0])
